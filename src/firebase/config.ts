5ce07411d1de30cfa2d18bd2e2f806764174accd
Purbanchal Hasta udyog <purbanchalhastaudyog@gmail.com>
2025-12-10 11:39:34 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2155773668-701c7",
+  "appId": "1:386183913384:web:ef4a51d261582afe7f879d",
+  "apiKey": "AIzaSyCx3OqrF2Rm0QWszTTunb-uTPudKRo4SsY",
+  "authDomain": "studio-2155773668-701c7.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "386183913384"
+};
