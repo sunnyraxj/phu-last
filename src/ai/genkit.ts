5d60b4de564deaa1e5fe1c1bb604f84efb4f3403
Purbanchal Hasta udyog <purbanchalhastaudyog@gmail.com>
2025-12-21 08:04:23 +0000

    use genai now for this api key

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -2,6 +2,6 @@ import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/google-genai';
 
 export const ai = genkit({
-  plugins: [googleAI({apiKey: process.env.GEMINI_API_KEY})],
+  plugins: [googleAI({apiKey: process.env.GENAI_API_KEY})],
   model: 'googleai/gemini-1.5-flash-latest',
 });
