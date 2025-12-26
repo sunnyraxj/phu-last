da039d0860a35ab3f30e5cc182da9c3bc9167eb8
Purbanchal Hasta udyog <purbanchalhastaudyog@gmail.com>
2025-12-26 08:58:16 +0000

    for mobile hero image only add top margin by 17%

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -532,7 +532,7 @@ export default function ProductPage() {
         </section>
 
         {/* Mobile Hero */}
-        <section className="md:hidden relative mt-[20%]">
+        <section className="md:hidden relative mt-[17%]">
           <div className="relative aspect-[16/9] w-full">
             <Image
                 src={siteSettings?.heroImageUrlMobile || siteSettings?.heroImageUrl || placeholderImages.heroMobile.url}
