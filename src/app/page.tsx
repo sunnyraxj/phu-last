f610c0e675a7080df46c1bb580a282ed4a84bc24
Purbanchal Hasta udyog <purbanchalhastaudyog@gmail.com>
2025-12-26 10:58:50 +0000

    for mobile make more compact like 3per row (_for element <FeaturesSectio

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -136,7 +136,7 @@ const FeaturesSection = () => {
                 >
                     <CarouselContent className="-ml-4">
                         {features.map((feature, index) => (
-                           <CarouselItem key={index} className="pl-4 basis-1/2 sm:basis-1/3 md:basis-1/4 lg:basis-1/5">
+                           <CarouselItem key={index} className="pl-4 basis-1/3 sm:basis-1/3 md:basis-1/4 lg:basis-1/5">
                                 <div className="flex flex-col items-center p-2 text-center">
                                     <feature.icon className="h-8 w-8 text-primary mb-2" />
                                     <h3 className="text-sm font-semibold text-foreground">{feature.title}</h3>
