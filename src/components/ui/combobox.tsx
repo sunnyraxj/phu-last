9f13be011bbd18f4af6f67ca966e3bc7b4851304
Purbanchal Hasta udyog <purbanchalhastaudyog@gmail.com>
2026-01-06 11:32:10 +0000

    why this not clickable (_for element <Primitive.div>_)

--- src/components/ui/combobox.tsx
+++ src/components/ui/combobox.tsx
@@ -68,9 +68,9 @@ export function Combobox({
               {options.map((option) => (
                 <CommandItem
                   key={option.value}
-                  value={option.label}
-                  onSelect={() => {
-                    onChange(option.value)
+                  value={option.value}
+                  onSelect={(currentValue) => {
+                    onChange(currentValue === value ? "" : currentValue)
                     setOpen(false)
                   }}
                 >
